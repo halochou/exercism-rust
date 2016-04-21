0c2114687c965e824e2b77debb637c8900556932
halochou <halo.chou@gmail.com>
2016-04-21 19:47:56 +0800

    minesweeper in progress

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -2,8 +2,15 @@ use std::collections::HashMap;
 
 pub fn annotate(board: &Vec<&str>) -> Vec<String> {
 	let mine_pos = get_mine_pos(board);
-	mine_pos.iter().fold(HashMap::new(), |acc, &(i,j)| {
-		if let Some(x) = i.checked_sub
+	mine_pos.iter().fold(HashMap::new(), |mut acc, &(i,j)| {
+		if let Some(x) = i.checked_sub(1) {
+			acc.insert((x, j)).or
+
+
+			if let Some(y) = j.checked_sub(1) {
+
+			}
+		}
 
 	})
 
